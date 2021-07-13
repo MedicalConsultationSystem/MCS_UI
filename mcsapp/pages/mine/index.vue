51680d10fa50b0fb89f1227c10d4902bacb3569f
vigour <vigour0402@gmail.com>
2021-07-14 01:10:08 +0800

    我的

--- mcsapp/pages/mine/index.vue
+++ mcsapp/pages/mine/index.vue
@@ -1,8 +1,92 @@
 <template>
+	<view>
+		<!-- 自定义导航栏 -->
+		<u-navbar :is-back="false" :background="background" height="40">
+			<view class="slot-wrap">
+				<view class="camera">
+					<u-icon name="camera-fill" color="#000000" size="52"></u-icon>
+				</view>
+			</view>
+		</u-navbar>
+		
+		<view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30">
+			<view class="u-m-r-10">
+				<u-avatar :src="pic" size="140"></u-avatar>
+			</view>
+			<view class="u-flex-1">
+				<view class="u-font-18 u-p-b-20">uView ui</view>
+				<view class="u-font-14 u-tips-color">微信号:helang_uView</view>
+			</view>
+			<view class="u-m-l-10 u-p-10">
+				<u-icon name="scan" color="#969799" size="28"></u-icon>
+			</view>
+			<view class="u-m-l-10 u-p-10">
+				<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
+			</view>
+		</view>
+		
+		<view class="u-m-t-20">
+			<u-cell-group>
+				<u-cell-item icon="rmb-circle" title="支付"></u-cell-item>
+			</u-cell-group>
+		</view>
+		
+		<view class="u-m-t-20">
+			<u-cell-group>
+				<u-cell-item icon="star" title="收藏"></u-cell-item>
+				<u-cell-item icon="photo" title="相册"></u-cell-item>
+				<u-cell-item icon="coupon" title="卡券"></u-cell-item>
+				<u-cell-item icon="heart" title="关注"></u-cell-item>
+			</u-cell-group>
+		</view>
+		
+		<view class="u-m-t-20">
+			<u-cell-group>
+				<u-cell-item icon="setting" title="设置"></u-cell-item>
+			</u-cell-group>
+		</view>
+	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				pic:'https://uviewui.com/common/logo.png',
+				show:true,
+				background: {
+					// backgroundColor: ''，
+					
+					// 导航栏背景图
+					// background: 'url(https://cdn.uviewui.com/uview/swiper/1.jpg) no-repeat',
+					// 还可以设置背景图size属性
+					// backgroundSize: 'cover',
+					
+					// 渐变色
+					backgroundImage: 'linear-gradient(45deg, rgb(28, 187, 180), rgb(141, 198, 63))'
+				}
+			}
+		},
+		onLoad() {
+			
+		},
+		methods: {
+			
+		}
+	}
 </script>
 
-<style>
+<style lang="scss">
+	.slot-wrap{
+		display: flex;
+		flex-direction: row-reverse;
+		align-items: center;
+		/* 如果您想让slot内容占满整个导航栏的宽度 */
+		flex: 1;
+		/* 如果您想让slot内容与导航栏左右有空隙 */
+		padding: 0 25rpx;
+	}
+	.user-box{
+		background-color: #fff;
+	}
 </style>

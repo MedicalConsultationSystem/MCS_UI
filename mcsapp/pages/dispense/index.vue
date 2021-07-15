30be9e4be265f79698ddc278918cd68b28b04d91
vigour <vigour0402@gmail.com>
2021-07-16 02:52:36 +0800

    复诊配药
    完成复诊配药上下滑动的动效切换

--- mcsapp/pages/dispense/index.vue
+++ mcsapp/pages/dispense/index.vue
@@ -1,11 +1,14 @@
 <template>
 	<view class="content">
 		<!-- 自定义导航栏 -->
-		<view class="wrap">
+		<view class="nav1" v-if="navShow1">
+			<u-navbar :is-back="true" back-icon-color="white" :title="title" title-color="white" :background="background" height="45"></u-navbar>
+		</view>
+		<view class="nav2" v-if="navShow2">
 			<u-navbar :is-back="true" back-icon-color="white" :title="title" title-color="white" :background="background" height="45"></u-navbar>
 		</view>
 		<!-- 选择医生面板 -->
-		<view class="doctorboard">
+		<view class="doctorboard" >
 			<u-card class="doctor" box-shadow="2px 3px 8px #888888">
 				<view class="doctor_head" slot="head">
 					<u-image width="150rpx" height="150rpx" :src="doctor.src" shape="circle"></u-image>
@@ -35,7 +38,7 @@
 			<view class="patient_item">
 				<text class="text1">确诊诊断</text>
 				<view>
-					<u-input placeholder="请填写病情诊断" v-model="patient.illness" style="text-align: right;"/>
+					<u-input placeholder="请填写病情诊断" v-model="patient.illness" style="width: 190rpx; text-align: right;"/>
 				</view>
 			</view>
 			<view class="patient_item2">
@@ -69,8 +72,8 @@
 			</view>
 		</view>
 		<!-- 提交 -->
-		<view >
-			<button class="btn" @click="show = true">提交</button>
+		<view class="push">
+			<button class="btn">提交</button>
 		</view>
 	</view>
 </template>
@@ -79,10 +82,12 @@
 	export default {
 		data() {
 			return {
+				navShow1: true,
+				navShow2: false,
 				show: false,
 				title: "复诊配药",
 				background: {
-					backgroundImage: 'rgba(79, 107, 208,0.95)'
+					backgroundImage: 'rgba(#ffffff,0)'
 				},
 				doctor: {
 					src:"../../static/touxiang/touxiang6.jpg"
@@ -96,22 +101,46 @@
 		onLoad() {
 			
 		},
+		onPageScroll({
+		  scrollTop
+		}) {
+		  if (scrollTop >= 50) {
+		    this.navShow1 = false;
+		    this.navShow2 = true;
+		  }
+		  if (scrollTop < 50) {
+		    this.navShow1 = true;
+		    this.navShow2 = false;
+		  }
+		},
 		methods: {
 			
 		},
 	}
 </script>
 
-<style>
-	.wrap{
-		background: linear-gradient(156deg, rgba(79, 107, 208,0.95));
+<style lang="scss" scoped>
+	// 导航栏样式切换
+	.nav1{
+		height: 350rpx;
+		width: 100%;
+		background: linear-gradient(156deg, rgba(79, 107, 208,1), rgb(98, 141, 185)45%, rgba(102, 175, 161,0.93)85%);
+		border-radius: 0 0 45rpx 45rpx;
+		position: fixed;
+		top: 0;
 	}
-	.content{
-		height: 330rpx;
-		background: linear-gradient(156deg, rgba(79, 107, 208,0.95), rgb(98, 141, 185)45%, rgba(102, 175, 161,0.93)85%);
-		border-radius: 0 0 40rpx 40rpx;
+	.nav2{
+		height: 110rpx;
+		width: 100%;
+		background: linear-gradient(156deg, rgba(79, 107, 208,1), rgb(98, 141, 185)45%, rgba(102, 175, 161,0.93)85%);
+		position: fixed;
+		top: 0;
+		z-index: 999;
 	}
-	
+	/* 选择医生 */
+	.doctorboard{
+		margin-top: 180rpx;
+	}
 	.doctor{
 		display: flex;
 		flex-direction: column;
@@ -162,7 +191,10 @@
 		margin-left: 20rpx;
 		color: #909399;
 	}
-	
+	.doctor_body{
+		margin-left: 20rpx;
+		font-size: 29rpx;
+	}
 	/* 病人信息 */
 	.patient_board{
 		display: flex;
@@ -242,14 +274,21 @@
 		border-bottom: 1rpx solid rgba(144,147,153,0.3);
 	}
 	.upimg{
-		margin-top: 20rpx;
-		margin-bottom: 20rpx;
+		margin-top: 50rpx;
+	}
+	.push{
+		display: flex;
+		flex-direction: row;
+		width: auto;
+		height: 80rpx;
+		align-items: center;
+		padding-bottom: 60rpx;
+		padding-top: 50rpx;
 	}
 	.btn{
 		width: 400rpx;
 		color: white;
 		background: rgba(79, 107, 208,0.9);
-		justify-content: center;
 		border-radius: 50rpx;
 		font-size: 35rpx;
 	 }

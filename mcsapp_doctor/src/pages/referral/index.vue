3eff06050d01dd5ab3111b22c209543ceeb2ada3
Stephen <31801162@stu.zucc.edu.cn>
2021-07-16 16:40:34 +0800

    新建复诊配药界面

--- mcsapp_doctor/src/pages/referral/index.vue
+++ mcsapp_doctor/src/pages/referral/index.vue
@@ -1,13 +1,118 @@
 <template>
+  <view>
+    <view>
+      <!-- 自定义导航栏 -->
+      <u-navbar :is-back="true" back-icon-color="white" :title="title" title-color="white" :background="background" height="45"></u-navbar>
+    </view>
+    <view class="doctorboard" >
+      <u-card class="doctor" box-shadow="2px 3px 8px #888888">
+        <view class="doctor_head" slot="head">
+          <u-image width="150rpx" height="150rpx" :src="doctor.src" shape="circle"></u-image>
+          <view class="name_text">
+            <text class="name_text1">方洪全</text>
+            <text class="name_text2">主任医师</text>
+          </view>
+          <view class="change">
+            <text class="change_text">更换医生</text>
+            <u-icon class="change_icon" name="arrow-right" size="30"></u-icon>
+          </view>
+        </view>
+        <view class="doctor_body" slot="body">
+          <text>呼吸内科</text>
+        </view>
+      </u-card>
+    </view>
+  </view>
 
 </template>
 
 <script>
 export default {
-name: "index"
+name: "index",
+  data(){
+  return{
+    title: "复诊配药",
+    background: {
+      backgroundImage: 'linear-gradient(156deg, rgba(79, 107, 208,0.95), rgb(98, 141, 185)45%, rgba(102, 175, 161,0.93)85%)'
+    },
+    doctor: {
+      src:"../../static/touxiang/touxiang6.jpg"
+    },
+    applyData:[
+      {
+        apply_time:"2021-7-16 15:41:31",
+        status:"待完成",
+        avatar:"../../static/touxiang/touxiang6.jpg",
+        name:"王大虎",
+        sex:"男",
+        age:18,
+        drugs:"lalalal"
+      }
+    ]
+  }
+
+}
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.doctorboard{
+  margin-top: 60rpx;
+}
+.doctor{
+  display: flex;
+  flex-direction: column;
+}
+.doctor_head{
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
+  align-items: center;
+}
+.name_text{
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+  margin-left: 40rpx;
+  font-size: 35rpx;
+}
+.name_text text{
+  margin-top: 8rpx;
+  margin-bottom: 8rpx;
+}
+.name_text1{
+  margin-left: 5rpx;
+  font-weight: bold;
+  font-size: 36rpx;
+}
+.name_text2{
+  border: 1rpx solid #ff9900;
+  border-radius: 30rpx;
+  font-size: 25rpx;
+  color: #ff9900;
+  padding-left: 10rpx;
+  padding-right: 10rpx;
+  padding-bottom: 4rpx;
+}
+.change{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-left: 150rpx;
+}
+.change_text{
+  margin-left: 15rpx;
+  font-size: 29rpx;
+  color: #909399;
+}
+.change_icon{
+  margin-left: 20rpx;
+  color: #909399;
+}
+.doctor_body{
+  margin-left: 20rpx;
+  font-size: 29rpx;
+}
 
 </style>

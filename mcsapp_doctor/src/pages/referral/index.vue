b06a482a8f9b60c305c4905b2c061957d0d452c5
Stephen <31801162@stu.zucc.edu.cn>
2021-07-16 19:25:12 +0800

    完成复诊配药病人卡片初步设计

--- mcsapp_doctor/src/pages/referral/index.vue
+++ mcsapp_doctor/src/pages/referral/index.vue
@@ -4,22 +4,36 @@
       <!-- 自定义导航栏 -->
       <u-navbar :is-back="true" back-icon-color="white" :title="title" title-color="white" :background="background" height="45"></u-navbar>
     </view>
-    <view class="doctorboard" >
-      <u-card class="doctor" box-shadow="2px 3px 8px #888888">
-        <view class="doctor_head" slot="head">
-          <u-image width="150rpx" height="150rpx" :src="doctor.src" shape="circle"></u-image>
+    <view class="patientboard" >
+      <u-card class="patient" box-shadow="2px 3px 8px #888888">
+        <view class="patient_head" slot="head">
+          <u-row class="head_row">
+            <span gutter="3">
+            <text class="apply_text">申请时间：{{applyData.apply_time}}</text>
+          </span>
+            <spn gutter="6">
+              <text class="status_text">{{applyData.status}}</text>
+            </spn>
+          </u-row>
+        </view>
+        <view class="patient_body" slot="body">
+          <u-image width="90rpx" height="90rpx" :src="patient.src" shape="circle"></u-image>
           <view class="name_text">
-            <text class="name_text1">方洪全</text>
-            <text class="name_text2">主任医师</text>
+            <u-row>
+              <text class="name_text1">{{applyData.name}}</text>
+              <text class="name_text1">{{applyData.sex}}</text>
+              <text class="name_text1">{{applyData.age}}岁</text>
+            </u-row>
+            <text class="name_text1">药品需求：{{applyData.drugs}}</text>
           </view>
           <view class="change">
-            <text class="change_text">更换医生</text>
             <u-icon class="change_icon" name="arrow-right" size="30"></u-icon>
           </view>
         </view>
-        <view class="doctor_body" slot="body">
-          <text>呼吸内科</text>
+        <view class="patient_foot" slot="foot">
+          <u-button size="default" >完成接诊</u-button>
         </view>
+
       </u-card>
     </view>
   </view>
@@ -35,20 +49,17 @@ name: "index",
     background: {
       backgroundImage: 'linear-gradient(156deg, rgba(79, 107, 208,0.95), rgb(98, 141, 185)45%, rgba(102, 175, 161,0.93)85%)'
     },
-    doctor: {
+    patient: {
       src:"../../static/touxiang/touxiang6.jpg"
     },
-    applyData:[
-      {
+    applyData: {
         apply_time:"2021-7-16 15:41:31",
         status:"待完成",
         avatar:"../../static/touxiang/touxiang6.jpg",
         name:"王大虎",
         sex:"男",
         age:18,
-        drugs:"lalalal"
-      }
-    ]
+        drugs:"lalalal"}
   }
 
 }
@@ -57,18 +68,15 @@ name: "index",
 
 <style lang="scss" scoped>
 
-.doctorboard{
-  margin-top: 60rpx;
+.patientboard{
+  margin-top: 40rpx;
 }
-.doctor{
+.patient{
   display: flex;
   flex-direction: column;
 }
-.doctor_head{
-  display: flex;
-  flex-direction: row;
-  justify-content: left;
-  align-items: center;
+.patient_head{
+
 }
 .name_text{
   display: flex;
@@ -84,7 +92,7 @@ name: "index",
 .name_text1{
   margin-left: 5rpx;
   font-weight: bold;
-  font-size: 36rpx;
+  font-size: 16rpx;
 }
 .name_text2{
   border: 1rpx solid #ff9900;
@@ -99,7 +107,13 @@ name: "index",
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-  margin-left: 150rpx;
+  margin-left: 260rpx;
+}
+.apply_text{
+  margin-left: 15rpx;
+  font-size: 20rpx;
+  width: 90rpx;
+  color: #909399;
 }
 .change_text{
   margin-left: 15rpx;
@@ -110,9 +124,25 @@ name: "index",
   margin-left: 20rpx;
   color: #909399;
 }
-.doctor_body{
-  margin-left: 20rpx;
-  font-size: 29rpx;
+.patient_body{
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
+  align-items: center;
+}
+.head_row{
+  width: 640rpx;
+}
+.patient_foot{
+  margin-left:520rpx;
+  font-size: 20rpx;
+  color: #4cd964;
+}
+.status_text{
+  margin-left: 249rpx;
+  font-size: 18rpx;
+  color: #909399;
+  padding: 20rpx;
 }
 
 </style>

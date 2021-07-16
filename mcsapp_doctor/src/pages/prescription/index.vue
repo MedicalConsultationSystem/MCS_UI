1ef1ca3630bd1faf89aa42782483b6733eef9c53
Stephen <31801162@stu.zucc.edu.cn>
2021-07-16 22:48:52 +0800

    添加药方界面全局选项卡

--- mcsapp_doctor/src/pages/prescription/index.vue
+++ mcsapp_doctor/src/pages/prescription/index.vue
@@ -1,13 +1,69 @@
 <template>
-
+    <view>
+      <view>
+        <!-- 自定义导航栏 -->
+        <u-navbar :is-back="true" back-icon-color="white" :title="title" title-color="white" :background="background" height="45"></u-navbar>
+      </view>
+      <view>
+        <view>
+          <u-tabs-swiper ref="uTabs" :list="list" :current="current" @change="tabsChange" :is-scroll="false"
+                         swiperWidth="750"></u-tabs-swiper>
+        </view>
+        <swiper :current="swiperCurrent" @transition="transition" @animationfinish="animationfinish">
+          <swiper-item class="swiper-item">
+            <scroll-view scroll-y style="height: 800rpx;width: 100%;" @scrolltolower="onreachBottom">
+              ...
+            </scroll-view>
+          </swiper-item>
+          <swiper-item class="swiper-item">
+            <scroll-view scroll-y style="height: 800rpx;width: 100%;" @scrolltolower="onreachBottom">
+              ...
+            </scroll-view>
+          </swiper-item>
+        </swiper>
+      </view>
+    </view>
 </template>
 
 <script>
 export default {
-name: "prescription"
+name: "prescription",
+  data() {
+    return {
+      title: "电子处方",
+      list: [{
+        name: '待提交'
+      }, {
+        name: '已开方'
+      }],
+      current:0,
+      swiperCurrent:0,
+      background: {
+        backgroundImage: 'linear-gradient(156deg, rgba(79, 107, 208,0.95), rgb(98, 141, 185)45%, rgba(102, 175, 161,0.93)85%)'
+      }
+    }
+  },
+  methods:{
+    tabsChange(index) {
+      this.swiperCurrent = index;
+    },
+    transition(e) {
+      let dx = e.detail.dx;
+      this.$refs.uTabs.setDx(dx);
+    },
+    animationfinish(e) {
+      let current = e.detail.current;
+      this.$refs.uTabs.setFinishCurrent(current);
+      this.swiperCurrent = current;
+      this.current = current;
+    },
+    onreachBottom() {
+
+    }
+  }
+
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
 </style>

47480dcbda7bb8af22a9b37ab5a4995f1d26ac80
Stephen <31801162@stu.zucc.edu.cn>
2021-07-17 09:39:39 +0800

    药品设置初始界面

--- mcsapp_doctor/src/pages/drugSetting/index.vue
+++ mcsapp_doctor/src/pages/drugSetting/index.vue
@@ -7,8 +7,33 @@
     <view>
       <u-form :model="form" ref="uForm">
         <u-form-item label="药品"><u-input v-model="form.sex" type="select" /></u-form-item>
-        <u-form-item label="剂量"><u-number-box v-model="value"></u-number-box></u-form-item>
+        <u-form-item label="剂量"><u-number-box v-model="form.dose" class="nb"></u-number-box></u-form-item>
+        <view class="">
+          <u-field @click="showFrequency" v-model="form.frequency_name"
+                   :disabled="true" label="频次" placeholder="请选择频次"
+                   right-icon="arrow-down-fill"
+          >
+          </u-field>
+          <u-action-sheet @click="clickFrequency" :list="frequency_nameList" v-model="showF"></u-action-sheet>
+        </view>
+        <view class="">
+          <u-field @click="showUsage" v-model="form.usage_name"
+                   :disabled="true" label="用法" placeholder="请选择用法"
+                   right-icon="arrow-down-fill"
+          >
+          </u-field>
+          <u-action-sheet @click="clickUsage" :list="usageList" v-model="showU"></u-action-sheet>
+        </view>
+        <u-form-item label="用药天数(天)"><u-number-box v-model="form.take_days" class="nb"></u-number-box></u-form-item>
+        <u-form-item label="总量(盒)"><u-number-box v-model="form.quantity" class="nb"></u-number-box></u-form-item>
+        <u-input placeholder="请输入备注(选填)" v-model="form.remark" :type="type" :border="true" class="remark_input" :auto-height=true />
       </u-form>
+    </view>
+    <view>
+      <u-row class="btn">
+        <u-button type="primary" u-icon="plus" >取消</u-button>
+        <u-button type="success">确定</u-button>
+      </u-row>
     </view>
   </view>
 </template>
@@ -18,20 +43,78 @@ export default {
 name: "drugSetting",
   data(){
   return{
+    usage_name: '',
+    usageList:[
+      {
+        text: '口服',
+      },
+      {
+        text: '外用'
+      },
+      {
+        text: '皮下注射'
+      },
+      {
+        text: '静脉注射'
+      }
+    ],
+    frequency_nameList:[
+      {
+        text: 'qd(每日一次)',
+      },
+      {
+        text: 'bid(每日两次)'
+      },
+      {
+        text: 'tiw(每周三次)'
+      },
+      {
+        text: '2w(每两周一次)'
+      }
+    ],
+    showU: false,
+    showF: false,
     form: {
-      name: '',
-      intro: '',
-      sex: ''
+      dose:"",
+      frequency_name:"",
+      usage_name:"",
+      take_days:"",
+      quantity:"",
+      sex: '',
+      remark:"",
     },
     value: 0,
     background: {
       backgroundImage: 'linear-gradient(156deg, rgba(79, 107, 208,0.95), rgb(98, 141, 185)45%, rgba(102, 175, 161,0.93)85%)'
     }
   }
+  },
+  methods:{
+    showFrequency() {
+      this.showF = true;
+    },
+    showUsage() {
+      this.showU = true;
+    },
+    clickUsage(index) {
+      this.form.usage_name = this.usageList[index].text;
+    },
+    clickFrequency(index) {
+      this.form.frequency_name = this.frequency_nameList[index].text;
+    }
   }
+
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.nb{
+  margin-left: 440rpx;
+}
+.remark_input{
+  height: 120rpx;
+}
+.btn{
+  margin-top: 230rpx;
+}
 </style>

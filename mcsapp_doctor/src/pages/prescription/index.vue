f09c63774eaaceae55ebdbede13e05daa21f58d5
Stephen <31801162@stu.zucc.edu.cn>
2021-07-25 14:14:33 +0800

    修复tab跳转及已开方高度问题

--- mcsapp_doctor/src/pages/prescription/index.vue
+++ mcsapp_doctor/src/pages/prescription/index.vue
@@ -4,111 +4,103 @@
         <!-- 自定义导航栏 -->
         <u-navbar back-icon-color="white" :title="title" title-color="white" :background="background" height="45"></u-navbar>
       </view>
-      <view>
-        <view>
-          <u-tabs-swiper ref="uTabs" :list="list" :current="current" @change="tabsChange" :is-scroll="false"
-                         swiperWidth="750"></u-tabs-swiper>
+      <view class="tabs">
+        <view class="tab_item" :class="{cur:index === tabCur}" v-for="(item, index) in ['待提交', '已开方']" :key="index" @click="tabSelect(index)">{{ item }}</view>
+      </view>
+      <!-- 标签页对应的内容 -->
+      <view v-if="tabCur === 0">
+        <view v-for="card in cards" :key="card.prescription_id">
+          <view class="prescriptionBoard">
+            <u-card class="prescription"  :head-border-bottom="false" :foot-border-bottom="false">
+              <view class="prescription_head" slot="head">
+                <view class="body">
+                  <text class="xi_text">西药方</text>
+                  <view class="add" @click="jumpToDrugAdd(card.prescription_id)">
+                    <u-icon name="plus" size="20"  class="icon" ></u-icon>
+                    <text class="add_text">新增药品</text>
+                  </view>
+                </view>
+              </view>
+              <view class="prescription_body" slot="body">
+                <block v-for="item in card.drugs" :key="item.prescription_drug_id" class="back_color">
+                  <view class="drug-information">
+                    <view class="body_left">
+                      <view class="factory_name">
+                        <text>{{item.drug_name}}</text>
+                      </view>
+                      <view >
+                        <text class="specification">{{item.specification}}</text>
+                      </view>
+                      <view >
+                        <text class="usage">{{usage}}</text>
+                        <text class="usage">{{item.usage_name}}</text>
+                      </view>
+                    </view>
+                    <view class="quantity">
+                      <text>{{item.quantity}}</text>
+                      <text>{{item.pack_unit}}</text>
+                    </view>
+                    <view class="trash_body" @click="delDrug(item.prescription_drug_id)">
+                      <u-icon name="trash" size="40rpx"></u-icon>
+                    </view>
+                  </view>
+                </block>
+              </view>
+              <view class="prescription_foot" slot="foot">
+                <view class="foot_trash" @click="delPrescription(card.prescription_id)" >
+                  <u-icon name="trash" size="40rpx"></u-icon>
+                </view>
+              </view>
+            </u-card>
+          </view>
         </view>
-        <swiper :current="swiperCurrent" style="height: 1440rpx;width: 100%;"@transition="transition" @animationfinish="animationfinish">
-          <swiper-item class="swiper-item">
-            <scroll-view scroll-y style="height: 1440rpx;width: 100%;" @scrolltolower="onreachBottom">
-              <view v-for="card in cards" :key="card.prescription_id">
-                <view class="prescriptionBoard">
-                  <u-card class="prescription"  :head-border-bottom="false" :foot-border-bottom="false">
-                    <view class="prescription_head" slot="head">
-                      <view class="body">
-                        <text class="xi_text">西药方</text>
-                        <view class="add" @click="jumpToDrugAdd(card.prescription_id)">
-                          <u-icon name="plus" size="20"  class="icon" ></u-icon>
-                          <text class="add_text">新增药品</text>
-                        </view>
-                      </view>
-                    </view>
-                    <view class="prescription_body" slot="body">
-                      <block v-for="item in card.drugs" :key="item.prescription_drug_id" class="back_color">
-                        <view class="drug-information">
-                          <view class="body_left">
-                            <view class="factory_name">
-                              <text>{{item.drug_name}}</text>
-                            </view>
-                            <view >
-                              <text class="specification">{{item.specification}}</text>
-                            </view>
-                            <view >
-                              <text class="usage">{{usage}}</text>
-                              <text class="usage">{{item.usage_name}}</text>
-                            </view>
-                          </view>
-                          <view class="quantity">
-                            <text>{{item.quantity}}</text>
-                            <text>{{item.pack_unit}}</text>
-                          </view>
-                          <view class="trash_body" @click="delDrug(item.prescription_drug_id)">
-                            <u-icon name="trash" size="40rpx"></u-icon>
-                          </view>
-                        </view>
-                      </block>
-                    </view>
-                    <view class="prescription_foot" slot="foot">
-                      <view class="foot_trash" @click="delPrescription(card.prescription_id)" >
-                        <u-icon name="trash" size="40rpx"></u-icon>
-                      </view>
-                    </view>
-                  </u-card>
+        <view>
+          <view class="btn">
+            <view class="add_btn">
+              <u-button type="primary" u-icon="plus" @click="addPrescription">新增处方</u-button>
+            </view>
+            <view class="submit_btn">
+              <u-button type="success" @click="sumPrescription">提交处方</u-button>
+            </view>
+          </view>
+        </view>
+      </view>
+      <view v-else-if="tabCur === 1">
+        <view v-for="card in submitedCards" :key="card.prescription_id">
+          <view class="prescriptionBoard">
+            <u-card class="prescription"  :head-border-bottom="false" :foot-border-bottom="false" show-foot="false">
+              <view class="prescription_head" slot="head">
+                <view class="body">
+                  <text class="xi_text">西药方</text>
                 </view>
               </view>
-              <view>
-                <view class="btn">
-                  <view class="add_btn">
-                    <u-button type="primary" u-icon="plus" @click="addPrescription">新增处方</u-button>
+              <view class="prescription_body" slot="body">
+                <block v-for="item in card.drugs" :key="item.prescription_drug_id" class="back_color">
+                  <view class="drug-information">
+                    <view class="body_left">
+                      <view class="factory_name">
+                        <text>{{item.drug_name}}</text>
+                        <text class="factory">{{item.factory_name}}</text>
+                      </view>
+                      <view >
+                        <text class="specification">{{item.specification}}</text>
+                      </view>
+                      <view >
+                        <text class="usage">{{usage}}</text>
+                        <text class="usage">{{item.usage_name}}</text>
+                      </view>
+                    </view>
+                    <view class="quantity">
+                      <text>{{item.quantity}}</text>
+                      <text>{{item.pack_unit}}</text>
+                    </view>
                   </view>
-                  <view class="submit_btn">
-                    <u-button type="success" @click="sumPrescription">提交处方</u-button>
-                  </view>
-                </view>
+                  <u-line class="line"></u-line>
+                </block>
               </view>
-            </scroll-view>
-          </swiper-item>
-          <swiper-item class="swiper-item">
-            <scroll-view scroll-y style="height: 800rpx;width: 100%;" @scrolltolower="onreachBottom">
-              <view v-for="card in submitedCards" :key="card.prescription_id">
-                <view class="prescriptionBoard">
-                  <u-card class="prescription"  :head-border-bottom="false" :foot-border-bottom="false" show-foot="false">
-                    <view class="prescription_head" slot="head">
-                      <view class="body">
-                        <text class="xi_text">西药方</text>
-                      </view>
-                    </view>
-                    <view class="prescription_body" slot="body">
-                      <block v-for="item in card.drugs" :key="item.prescription_drug_id" class="back_color">
-                        <view class="drug-information">
-                          <view class="body_left">
-                            <view class="factory_name">
-                              <text>{{item.drug_name}}</text>
-                              <text class="factory">{{item.factory_name}}</text>
-                            </view>
-                            <view >
-                              <text class="specification">{{item.specification}}</text>
-                            </view>
-                            <view >
-                              <text class="usage">{{usage}}</text>
-                              <text class="usage">{{item.usage_name}}</text>
-                            </view>
-                          </view>
-                          <view class="quantity">
-                            <text>{{item.quantity}}</text>
-                            <text>{{item.pack_unit}}</text>
-                          </view>
-                        </view>
-                        <u-line class="line"></u-line>
-                      </block>
-                    </view>
-                  </u-card>
-                </view>
-              </view>
-            </scroll-view>
-          </swiper-item>
-        </swiper>
+            </u-card>
+          </view>
+        </view>
       </view>
     </view>
 </template>
@@ -121,6 +113,7 @@ name: "prescription",
       description:"患者所需药品",
       title: "电子处方",
       index:0,
+      tabCur: 0,
       rev:null,
       receive:null,
       cards:[],
@@ -189,6 +182,10 @@ name: "prescription",
     }
   },
   methods:{
+    tabSelect(index) {
+      if (this.tabCur === index) return
+      this.tabCur = index
+    },
     delPrescription(index){
       this.delPrescriptionId.prescription_id=parseInt(index)
       let reqJSON=JSON.stringify(this.delPrescriptionId);
@@ -252,6 +249,7 @@ name: "prescription",
           console.log("处方查询成功！")
           console.log(res.data.data)
           this.cards=[]
+          this.submitedCards=[]
           for(let item in res.data.data.prescriptions ){
             if(res.data.data.prescriptions[item].prescription_status==="0"){
               this.cards.push(res.data.data.prescriptions[item])
@@ -340,6 +338,34 @@ name: "prescription",
 .drug_name{
   font-size: 27rpx;
   color: #303133;
+}
+.tabs {
+  display: flex;
+  justify-content: space-evenly;
+  font-size: 32rpx;
+  background-color: #FFF;
+  color: #A9A9A9;
+}
+
+.tab_item {
+  height: 114rpx;
+  line-height: 100rpx;
+
+  &.cur {
+    position: relative;
+    color: #F62D5C;
+
+    &:after {
+      content: '';
+      width: 36rpx;
+      height: 6rpx;
+      position: absolute;
+      bottom: 20rpx;
+      left: 50%;
+      margin-left: -18rpx;
+      background-color: #F62D5C;
+    }
+  }
 }
 .header {
   width: 100%;
